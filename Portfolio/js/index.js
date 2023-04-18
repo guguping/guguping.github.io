c05d48231b02a137ac809b783f6b342db6cb60dc
guguping <qkfhandur@naver.com>
2023-04-18 11:08:37 +0900

    button_hover

--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -8,35 +8,35 @@ const goLogin = () => {
   location.href = "login.html";
 };
 
-const button_nc1 = () => {
-  const onbutton = document.getElementById("button-1");
-  onbutton.className = "btn btn-success";
-};
-const button_og1 = () => {
-  const onbutton = document.getElementById("button-1");
-  onbutton.className = "btn btn-warning";
-};
-const button_nc2 = () => {
-  const onbutton = document.getElementById("button-2");
-  onbutton.className = "btn btn-success";
-};
-const button_og2 = () => {
-  const onbutton = document.getElementById("button-2");
-  onbutton.className = "btn btn-warning";
-};
-const button_nc3 = () => {
-  const onbutton = document.getElementById("button-3");
-  onbutton.className = "btn btn-success";
-};
-const button_og3 = () => {
-  const onbutton = document.getElementById("button-3");
-  onbutton.className = "btn btn-warning";
-};
-const button_nc4 = () => {
-  const onbutton = document.getElementById("button-4");
-  onbutton.className = "btn btn-success";
-};
-const button_og4 = () => {
-  const onbutton = document.getElementById("button-4");
-  onbutton.className = "btn btn-warning";
-};
+// const button_nc1 = () => {
+//   const onbutton = document.getElementById("button-1");
+//   onbutton.className = "btn btn-success";
+// };
+// const button_og1 = () => {
+//   const onbutton = document.getElementById("button-1");
+//   onbutton.className = "btn btn-warning";
+// };
+// const button_nc2 = () => {
+//   const onbutton = document.getElementById("button-2");
+//   onbutton.className = "btn btn-success";
+// };
+// const button_og2 = () => {
+//   const onbutton = document.getElementById("button-2");
+//   onbutton.className = "btn btn-warning";
+// };
+// const button_nc3 = () => {
+//   const onbutton = document.getElementById("button-3");
+//   onbutton.className = "btn btn-success";
+// };
+// const button_og3 = () => {
+//   const onbutton = document.getElementById("button-3");
+//   onbutton.className = "btn btn-warning";
+// };
+// const button_nc4 = () => {
+//   const onbutton = document.getElementById("button-4");
+//   onbutton.className = "btn btn-success";
+// };
+// const button_og4 = () => {
+//   const onbutton = document.getElementById("button-4");
+//   onbutton.className = "btn btn-warning";
+// };
